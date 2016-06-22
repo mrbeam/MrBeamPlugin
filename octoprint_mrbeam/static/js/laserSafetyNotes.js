03d740ffa2fc8830e6c49c495dc4d5a77ae15c3a
make-ing <bckr.florian@gmail.com>
2016-06-22 16:26:38 +0200

    removed all settings related stuff because we don't need it here.

--- octoprint_mrbeam/static/js/laserSafetyNotes.js
+++ octoprint_mrbeam/static/js/laserSafetyNotes.js
@@ -1,21 +1,10 @@
 $(function() {
     
-	function LaserSafetyNotesViewModel(params) {
+	function LaserSafetyNotesViewModel() {
 		var self = this;
-
-		self.settings = params[0];
-		self.s = ko.observable(self.settings.settings);
-		
-		self.agreed_to_safety_notes = ko.computed(function(){
-//			console.log(self.s());
-//			if(self.s && self.s().plugins)
-//					return self.s.plugins.laser_safety_notes;
-			return false;
-		});
 
 		self.onStartup = function(){
 			$('#laser_safety_overlay').modal("show");
-//			console.log("settings", self.settings.settings);
 		};
 		
 		self.agree = function(){
@@ -26,7 +15,7 @@ $(function() {
 	
     // view model class, identifier, parameters for constructor, container to bind to
     ADDITIONAL_VIEWMODELS.push([LaserSafetyNotesViewModel,
-		["settingsViewModel"], 
+		[], 
 		document.getElementById("laser_safety_overlay")]);
 	
 });
