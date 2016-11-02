18a3d5d4e0e93f1ddf2593b590734069b97d6e40
make-ing <bckr.florian@gmail.com>
2016-11-02 12:12:38 +0100

    changed coordinate origin for prototype B after homing to x=500 and y=400

--- octoprint_mrbeam/printer.py
+++ octoprint_mrbeam/printer.py
@@ -3,7 +3,7 @@ from octoprint.settings import settings
 from . import comm_acc2 as comm
 
 class Laser(Printer):
-	
+
 	def __init__(self, fileManager, analysisQueue, printerProfileManager):
 		Printer.__init__(self, fileManager, analysisQueue, printerProfileManager)
 		self._stateMonitor = LaserStateMonitor(
@@ -32,7 +32,7 @@ class Laser(Printer):
 			progress={"completion": None, "filepos": None, "printTime": None, "printTimeLeft": None},
 			current_z=None
 		)
-		
+
 	# overwrite connect to use comm_acc2
 	def connect(self, port=None, baudrate=None, profile=None):
 		"""
@@ -47,16 +47,16 @@ class Laser(Printer):
 	# overwrite operational state to accept commands in locked state
 	def is_operational(self):
 		return Printer.is_operational(self) or self.is_locked()
-		
+
 	# extend commands: home, position, increase_passes, decrease_passes
 	def home(self, axes):
-		self.commands(["$H", "G92X0Y0Z0", "G90", "G21"])
+		self.commands(["$H", "G92X500Y400Z0", "G90", "G21"])
 
 	def position(self, x, y):
 		printer_profile = self._printerProfileManager.get_current_or_default()
 		movement_speed = min(printer_profile["axes"]["x"]["speed"], printer_profile["axes"]["y"]["speed"])
 		self.commands(["G90", "G0 X%.3f Y%.3f F%d" % (x, y, movement_speed)])
-	
+
 	def increase_passes(self):
 		"""
 		 increase the number of passes by one.
@@ -78,7 +78,7 @@ class Laser(Printer):
 			return
 		self._comm.decreasePasses()
 
-	
+
 	# extend flags
 	def is_locked(self):
 		return self._comm is not None and self._comm.isLocked()
@@ -93,7 +93,7 @@ class Laser(Printer):
 			"flashing": self.is_flashing(),
 		})
 		return flags
-	
+
 	# position update callbacks
 	def on_comm_pos_update(self, MPos, WPos):
 		self._add_position_data(MPos, WPos)
@@ -109,7 +109,7 @@ class Laser(Printer):
 		self._stateMonitor.setMachinePosition(MPos)
 
 
-class LaserStateMonitor(StateMonitor):	
+class LaserStateMonitor(StateMonitor):
 	def __init__(self, *args, **kwargs):
 		StateMonitor.__init__(self, *args, **kwargs)
 		self._machinePosition = None
@@ -130,4 +130,3 @@ class LaserStateMonitor(StateMonitor):
 			"machinePosition": self._machinePosition
 		})
 		return data
-	
