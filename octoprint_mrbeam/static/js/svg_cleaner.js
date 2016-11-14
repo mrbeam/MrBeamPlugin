956c7243a60dc9bb99da10276d6e86fb136fdd31
make-ing <bckr.florian@gmail.com>
2016-11-14 18:03:56 +0100

    added first few clean up functions

--- octoprint_mrbeam/static/js/svg_cleaner.js
+++ octoprint_mrbeam/static/js/svg_cleaner.js
@@ -14,33 +14,124 @@
 //    You should have received a copy of the GNU Affero General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
-
 Snap.plugin(function (Snap, Element, Paper, global) {
 
-	/**
-	 * bakes transformations of the element and all sub-elements into coordinates
-	 *
-	 * @param {boolean} toCubics : use only cubic path segments
-	 * @param {integer} dec : number of digits after decimal separator. defaults to 5
-	 * @returns {undefined}
-	 */
-	Element.prototype.clean = function () {
-		var elem = this;
-		if (!elem || !elem.paper){
-  			return;
-        } // don't handle unplaced elements. this causes double handling.
+    /**
+     * deletes all unnecessary elements and normalizes other stuff
+     *
+     * @param {none}
+     * @returns {undefined}
+     */
+    Element.prototype.clean = function () {
+        var elem = this;
 
-		var children = elem.children();
-		if (children.length > 0) {
-			for (var i = 0; i < children.length; i++) {
-				var child = children[i];
-				child.clean();
-			}
-			return;
-		}
+        var children = elem.children();
+        if (children.length > 0) {
+            for (var i = 0; i < children.length; i++) {
+                var child = children[i];
+                child.clean();
+            }
+        }
 
-        console.log(elem)
+        if (elem.type !== "path" &&
+            elem.type !== "circle" &&
+            elem.type !== "ellipse" &&
+            elem.type !== "rect" &&
+            elem.type !== "line" &&
+            elem.type !== "polyline" &&
+            elem.type !== "path") {
+            return;
+        }
 
-	};
+        for (var property in elem.attr()) {
+            if (elem.attr().hasOwnProperty(property)) {
+                if (property.includes(":")) { // loescht alle attribute mit namespace anderst als svg
+                    elem.attr(property, "");
+                } else if (property == "style") { // entpackt den style attribute und entfernt default Werte
+                    elem.unwrap_style_attr()
+                }
+            }
+        }
+
+        if (!elem.attr().hasOwnProperty("stroke")) { // fuegt das stroke attribute in das element ein wenn nicht gesestzt
+            elem.attr("stroke", elem.attr("stroke")); // TODO die passende group finden und stroke dort entfernen.
+        }
+    };
+
+    /**
+    * extracts all non default style attributes and deletes it afterwards.
+    *
+    * @param {none}
+    * @returns {undefined}
+    */
+    Element.prototype.unwrap_style_attr = function () {
+        var elem = this;
+
+        var defaults = {
+            'baseline-shift': 'baseline',
+            'clip-path': 'none',
+            'clip-rule': 'nonzero',
+            'color': '#000',
+            'color-interpolation-filters': 'linearRGB',
+            'color-interpolation': 'sRGB',
+            'direction': 'ltr',
+            'display': 'inline',
+            'enable-background': 'accumulate',
+            'fill': '#000',
+            'fill-opacity': '1',
+            'fill-rule': 'nonzero',
+            'filter': 'none',
+            'flood-color': '#000',
+            'flood-opacity': '1',
+            'font-size-adjust': 'none',
+            'font-size': 'medium',
+            'font-stretch': 'normal',
+            'font-style': 'normal',
+            'font-variant': 'normal',
+            'font-weight': 'normal',
+            'glyph-orientation-horizontal': '0deg',
+            'letter-spacing': 'normal',
+            'lighting-color': '#fff',
+            'marker': 'none',
+            'marker-start': 'none',
+            'marker-mid': 'none',
+            'marker-end': 'none',
+            'mask': 'none',
+            'opacity': '1',
+            'pointer-events': 'visiblePainted',
+            'stop-color': '#000',
+            'stop-opacity': '1',
+            'stroke': 'none',
+            'stroke-dasharray': 'none',
+            'stroke-dashoffset': '0',
+            'stroke-linecap': 'butt',
+            'stroke-linejoin': 'miter',
+            'stroke-miterlimit': '4',
+            'stroke-opacity': '1',
+            'stroke-width': '1',
+            'text-anchor': 'start',
+            'text-decoration': 'none',
+            'unicode-bidi': 'normal',
+            'visibility': 'visible',
+            'word-spacing': 'normal',
+            'writing-mode': 'lr-tb',
+            // SVG 1.2 tiny properties
+            'audio-level': '1',
+            'solid-color': '#000',
+            'solid-opacity': '1',
+            'text-align': 'start',
+            'vector-effect': 'none',
+            'viewport-fill': 'none',
+            'viewport-fill-opacity': '1'
+        };
+
+        var style = elem.attr("style");
+        style.split(";").forEach(function (item, index) {
+            attr = item.split(":");
+            if (attr[0] in defaults && defaults[attr[0]] != attr[1]) {
+                elem.attr(attr[0], attr[1]);
+                elem.attr("style", "");
+            }
+        })
+    };
 });
