cc5ca39055b3503855ad82cfb8153aae3ae80338
clemniem <clemens.niemeyer@gmail.com>
2016-12-08 13:43:12 +0100

    simplify

--- octoprint_mrbeam/static/js/render_fills.js
+++ octoprint_mrbeam/static/js/render_fills.js
@@ -52,7 +52,7 @@ Snap.plugin(function (Snap, Element, Paper, global) {
 				selection.push(elem);
 			} else {
 				if(fillPaths && elem.is_filled()){
-					selection.push(elem.attr({style:"stroke: none; "+elem.attr('style')}));
+					selection.push(elem);
 				} else {
 					elem.remove();
 				}
