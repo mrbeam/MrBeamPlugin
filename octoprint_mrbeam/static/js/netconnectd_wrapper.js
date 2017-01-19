9678b0d7d3e57e245fffa245d1b6569e4cb73734
Andy Werner <andy@mr-beam.org>
2017-01-19 16:40:47 +0000

    First version of a working wifi wizard. Still many bugs and flaws. However the golden path works.

--- octoprint_mrbeam/static/js/netconnectd_wrapper.js
+++ octoprint_mrbeam/static/js/netconnectd_wrapper.js
@@ -2,6 +2,9 @@ $(function() {
     function NetconnectdWrapperViewModel(parameters) {
         var self = this;
 		self.netconnectdViewModel = parameters[0];
+		
+		// Disable paging. Scrolling is the new paging...
+		self.netconnectdViewModel.listHelper.pageSize(0);
 	}
 	
     // view model class, parameters for constructor, container to bind to
