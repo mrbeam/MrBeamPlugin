edefcf1fe610be998a75dc79d601cc0d764f87fd
Teja <mail@tejaphilipp.de>
2017-01-23 22:50:17 +0100

    gc embedding works with progress.

--- octoprint_mrbeam/static/js/snap_gc_plugin.js
+++ octoprint_mrbeam/static/js/snap_gc_plugin.js
@@ -21,27 +21,44 @@ Snap.plugin(function (Snap, Element, Paper, global) {
 	/**
 	 * generates gc from d attr
 	 * 
+	 * @param {float} correction_matrix : matrix to be applied on resulting points
 	 * @param {float} max_derivation : how precise curves are approximated
 	 * @param {float} max_segment_length : maximum length of G1 commands
 	 * @returns {undefined}
 	 */
-	Element.prototype.generate_gc = function (max_derivation, max_segment_length) {
+	Element.prototype.generate_gc = function (correction_matrix, max_derivation, min_segment_length, max_segment_length) {
 		var elem = this;
 		
 		var max_derivation = max_derivation || .1; // TODO - real dist, not manhattan dist.
+		var min_segment_length = min_segment_length || .1; 
 		var max_segment_length = max_segment_length || 10; 
+
+		if(max_segment_length < min_segment_length){
+			max_segment_length = min_segment_length;
+			console.warn("max_segment_length can't be smaller than min_segment_length!");
+		}
 
 		if(elem.type !== 'path'){
 			console.log('Only path elemts are supported. This is ', elem.type);
 			return;
 		}
 
-		var points = elem.approximateArray(0, elem.getTotalLength(), max_derivation);
+		var transformed_path_array = Snap.path.map(elem.attr('d'), correction_matrix);
+		var temp = elem.paper.path(transformed_path_array).attr('opacity',0);
 
+		var length = temp.getTotalLength();
+		var progress_callback = function(position){ 
+			console.log('progress', position/length); 
+		};
+		var points = temp.approximateArray(0, length, max_derivation, min_segment_length, max_segment_length, progress_callback);
+		
+		temp.remove();
 		var gc = 'G0X'+points[0].x.toFixed(2)+'Y'+points[0].y.toFixed(2)+"\n";
 		for (var i = 1; i < points.length; i++) {
 			var p = points[i];
 			gc += 'G1X'+p.x.toFixed(2)+'Y'+p.y.toFixed(2)+"\n";
+//			DEBUG visualization
+			elem.parent().append(elem.paper.circle(p.x, p.y, 1.0).attr({fill:'green'}));
 		}
 		elem.attr('gc', gc);
 	};
@@ -50,8 +67,7 @@ Snap.plugin(function (Snap, Element, Paper, global) {
 
 
 		
-	Element.prototype.approximateArray = function(start, end, max_derivation){
-		console.log("approx", start, end);
+	Element.prototype.approximateArray = function(start, end, max_derivation, min_segment_length, max_segment_length, progress){
 		var elem = this;
 		if(start === undefined) start = 0;
 		if(end === undefined) end = elem.getTotalLength();
@@ -60,35 +76,40 @@ Snap.plugin(function (Snap, Element, Paper, global) {
 		var center = start + length/2;
 		var start_point = elem.getPointAtLength(start);
 		var end_point = elem.getPointAtLength(end);
-		var angle_rad = start_point.alpha * Math.PI/180;
-		var projection_point_x = start_point.x - length * Math.cos(angle_rad);
-		var projection_point_y = start_point.y - length * Math.sin(angle_rad);
+//		var angle_rad = start_point.alpha * Math.PI/180;
+		var projection_point_x = start_point.x - length * Snap.cos(start_point.alpha);
+		var projection_point_y = start_point.y - length * Snap.sin(start_point.alpha);
 //		DEBUG visualization
 //		elem.parent().append(elem.paper.circle(start_point.x,start_point.y, 0.3).attr({fill:'blue'}));
 //		elem.parent().append(elem.paper.circle(end_point.x, end_point.y, 0.3).attr({fill:'cyan'}));
 //		elem.parent().append(elem.paper.circle(projection_point_x, projection_point_y, 0.3).attr({fill:'red'}));
 		
 		var subdivide = false;
-		if(Math.abs(projection_point_x - end_point.x) > max_derivation){
-			subdivide = true;
-		} else {
-			if(Math.abs(projection_point_y - end_point.y) > max_derivation){
+		if(length > min_segment_length*2){ // divide segment only if long enough
+			if(length > max_segment_length){
 				subdivide = true;
+			}
+			if(Math.abs(projection_point_x - end_point.x) > max_derivation ||
+				Math.abs(projection_point_y - end_point.y) > max_derivation){ // Manhattan dist
+				subdivide = true;
+			} else { // real euclidean dist
+				if(Math.pow(projection_point_x - end_point.x,2) + Math.pow(projection_point_y - end_point.y,2) > Math.pow(max_derivation,2)){
+					subdivide = true;
+				} 
 			}
 		}
 		var points = [];
 		if(subdivide){
-			points = elem.approximateArray(start, center, max_derivation)
-					.concat(elem.approximateArray(center, end, max_derivation));
+			var points_first_half = elem.approximateArray(start, center, max_derivation, min_segment_length, max_segment_length, progress);
+			var points_second_half = elem.approximateArray(center, end, max_derivation, min_segment_length, max_segment_length, progress); 
+			points_second_half.shift(); // drop first, it is the last from the first_half
+			points = points_first_half.concat(points_second_half);
 		} else {
 			points = [start_point, end_point];
 		}
-		
-//		DEBUG visualization
-//		for (var idx = 0; idx < points.length; idx++) {
-//			var p = points[idx];
-//			elem.parent().append(elem.paper.circle(p.x, p.y, 0.4).attr({fill:'green'}));
-//		}
+		if(typeof progress === 'function'){
+			progress(end);
+		}
 		return points;
 	};
 	
