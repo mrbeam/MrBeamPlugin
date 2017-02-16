f2aa557faaf302c5fd854518f6da0628e709624b
Andy Werner <andy@mr-beam.org>
2017-02-16 16:10:10 +0100

    DEBUG: Tried to move font links into the SVG so that the conversion process has them. But didn't work. You may want to remove these changes

--- octoprint_mrbeam/__init__.py
+++ octoprint_mrbeam/__init__.py
@@ -128,7 +128,8 @@ class MrBeamPlugin(octoprint.plugin.SettingsPlugin,
 			js=["js/lasercutterprofiles.js","js/mother_viewmodel.js", "js/mrbeam.js","js/color_classifier.js","js/working_area.js", "js/camera.js",
 			"js/lib/snap.svg-min.js", "js/render_fills.js", "js/path_convert.js", "js/matrix_oven.js", "js/drag_scale_rotate.js",
 			"js/convert.js", "js/gcode_parser.js", "js/lib/photobooth_min.js", "js/laserSafetyNotes.js", "js/svg_cleaner.js", "js/corewizard.js"],
-			css=["css/mrbeam.css", "css/svgtogcode.css", "css/ui_mods.css", "css/quicktext-fonts.css"],
+			# css=["css/mrbeam.css", "css/svgtogcode.css", "css/ui_mods.css", "css/quicktext-fonts.css"],
+			css=["css/mrbeam.css", "css/svgtogcode.css", "css/ui_mods.css"],
 			less=["less/mrbeam.less"]
 		)
 
