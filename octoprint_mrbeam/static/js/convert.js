3c68549fd2cde85e0a583a8622dca20db6e81cfb
Teja <mail@tejaphilipp.de>
2017-05-21 11:01:35 +0200

    new visualization of lines and filled shapes in conversion dialog

--- octoprint_mrbeam/static/js/convert.js
+++ octoprint_mrbeam/static/js/convert.js
@@ -95,12 +95,14 @@ $(function(){
 		};
 
 		self._getColorIcon = function(color){
-			var i = $('<div/>',{
+			var i = $('<img />',{
 				id: 'cd_color_'+color.hex.substr(1),
 				style: "background-color: "+color.hex+";",
 				draggable: "true",
 				class: 'used_color'
-			}).on({
+			})
+			.attr({src: "/plugin/mrbeam/static/img/line_overlay.svg"})
+			.on({
 				dragstart: function(ev){ colorDragStart(ev.originalEvent); },
 				dragend: function(ev){ colorDragEnd(ev.originalEvent); }
 			});
