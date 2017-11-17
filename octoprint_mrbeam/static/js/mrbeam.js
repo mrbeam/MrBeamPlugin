617e80bed160b634143a02eb99f220c70f63ae98
Andy Werner <andy@mr-beam.org>
2017-11-17 17:43:14 +0100

    Fixes #221: Not working in Safari. Actually, this is more a quickfix. I couldn't figure out how handle load event on svg image in safari. But this should be close to perfect anyways.

--- octoprint_mrbeam/static/js/mrbeam.js
+++ octoprint_mrbeam/static/js/mrbeam.js
@@ -4,6 +4,21 @@
  * Author: Teja
  * License: AGPLv3
  */
+
+/**
+    browser detection code
+    https://stackoverflow.com/a/7768006/2631798
+    Might not be perfect, but for now it's ok.
+ */
+var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
+var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
+var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+var is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
+if ((is_chrome)&&(is_safari)) {is_safari=false;}
+if ((is_chrome)&&(is_opera)) {is_chrome=false;}
+
+
 $(function() {
     function MrbeamViewModel(parameters) {
         var self = this;
