mrbeam/MrBeamPlugin

commit 9ed9a2b2194c4523b5c662a42e53c04b226a0477

LoadingOverlayViewModel -> introducing reload overlay also: general scroll prevention on body level

octoprint_mrbeam/static/js/loadingoverlay_viewmodel.js
@@ -1,0 +1,39 @@
+
+
+$(function() {
+    function LoadingOverlayViewModel(parameters) {
+        var self = this;
+
+        self.TEXT_RELOADING = 'beamOS is reloading...';
+
+        self.display_state = ko.observable($('#loading_overlay_message').text());
+
+        window.onbeforeunload = function(){
+            self.showReloading();
+        };
+
+        self.removeLoadingOverlay = function(){
+            $('#loading_overlay').hide();
+            console.log("beamOS started. overlay removed.");
+            console.log("%c      ", "color: transparent; font-size: 150px; background:url('http://www.mr-beam.org/img/logo2_path.svg') no-repeat bottom left");
+        };
+
+        self.showReloading = function(){
+            self.display_state(self.TEXT_RELOADING);
+            $('#loading_overlay').show();
+        };
+
+
+    };
+
+    // view model class, parameters for constructor, container to bind to
+    OCTOPRINT_VIEWMODELS.push([
+        LoadingOverlayViewModel,
+
+        // e.g. loginStateViewModel, settingsViewModel, ...
+        [ /* "loginStateViewModel", "settingsViewModel" */ ],
+
+        // e.g. #settings_plugin_mrbeam, #tab_plugin_mrbeam, ...
+        [ document.getElementById("loading_overlay") ]
+    ]);
+});

octoprint_mrbeam/__init__.py
@@ -294,7 +294,7 @@ class MrBeamPlugin(octoprint.plugin.SettingsPlugin,
 				"js/lib/photobooth_min.js", "js/svg_cleaner.js", "js/loginscreen_viewmodel.js",
 				"js/wizard_acl.js", "js/netconnectd_wrapper.js", "js/lasersaftey_viewmodel.js",
 				"js/ready_to_laser_viewmodel.js", "js/lib/screenfull.min.js","js/settings/camera_calibration.js",
-				"js/path_magic.js", "js/lib/simplify.js", "js/lib/clipper.js", "js/laser_job_done_viewmodel.js"],
+				"js/path_magic.js", "js/lib/simplify.js", "js/lib/clipper.js", "js/laser_job_done_viewmodel.js", "js/loadingoverlay_viewmodel.js"],
 			css=["css/mrbeam.css", "css/svgtogcode.css", "css/ui_mods.css", "css/quicktext-fonts.css", "css/sliders.css"],
 			less=["less/mrbeam.less"]
 		)

octoprint_mrbeam/static/js/mother_viewmodel.js
@@ -15,6 +15,7 @@ $(function () {
         self.readyToLaser = params[10];
         self.navigation = params[11];
         self.appearance = params[12];
+        self.loadingOverlay = params[13];
 
         self.isStartupComplete = false;
         self.storedSocketData = [];
@@ -240,9 +241,7 @@ $(function () {
 
         self.removeLoadingOverlay = function(){
             if (self.isStartupComplete &&  self.workingArea.camera.firstImageLoaded) {
-                $('#loading_overlay').remove();
-                console.log("beamOS started. overlay removed.");
-                console.log("%c      ", "color: transparent; font-size: 150px; background:url('http://www.mr-beam.org/img/logo2_path.svg') no-repeat bottom left");
+                self.loadingOverlay.removeLoadingOverlay();
             } else {
                 setTimeout(self.removeLoadingOverlay, 100);
             }
@@ -566,7 +565,8 @@ $(function () {
     ADDITIONAL_VIEWMODELS.push([MotherViewModel,
         ["loginStateViewModel", "settingsViewModel", "printerStateViewModel", "filesViewModel", "gcodeFilesViewModel",
             "connectionViewModel", "controlViewModel", "terminalViewModel", "workingAreaViewModel",
-            "vectorConversionViewModel", "readyToLaserViewModel", "navigationViewModel", "appearanceViewModel"],
+            "vectorConversionViewModel", "readyToLaserViewModel", "navigationViewModel", "appearanceViewModel",
+            "loadingOverlayViewModel"],
         [document.getElementById("mrb_state"),
             document.getElementById("mrb_control"),
             document.getElementById("mrb_connection_wrapper"),

octoprint_mrbeam/templates/loading_overlay.jinja2
@@ -1,9 +1,6 @@
 
 <div id="loading_overlay">
     <style>
-        body {
-            overflow: hidden;
-        }
 
         #loading_overlay {
             position: fixed;
@@ -16,7 +13,7 @@
             background-color: #ffffff;
         }
 
-        .background {
+        .loading_overlay_background {
             position: fixed;
             top: 0;
             left: 0;
@@ -24,18 +21,18 @@
             height: 100%;
         }
 
-        .text{
+        .loading_overlay_text{
             margin:0;
             font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;
             font-size:14px;
             line-height:20px;
         }
 
-        .red {
+        .loading_overlay_red {
             color: #e25303;
         }
 
-        .wrapper {
+        .loading_overlay_wrapper {
             position: absolute;
             top: 0;
             bottom: 0;
@@ -43,23 +40,23 @@
             right: 0;
         }
 
-        .wrapper .outer {
+        .loading_overlay_wrapper .loading_overlay_outer {
             display: table;
             width: 100%;
             height: 100%;
         }
 
-        .wrapper .outer .inner {
+        .loading_overlay_wrapper .loading_overlay_outer .loading_overlay_inner {
             display: table-cell;
             vertical-align: middle;
             padding: 20px;
         }
 
-        .wrapper .outer .inner .content {
+        .loading_overlay_wrapper .loading_overlay_outer .loading_overlay_inner .loading_overlay_content {
             text-align: center;
         }
 
-        .pulsate3 {
+        .loading_overlay_pulsate3 {
             -webkit-animation: pulsate 1s ease-out;
             -webkit-animation-iteration-count: infinite;
             opacity: 0.1;
@@ -77,13 +74,13 @@
             }
         }
     </style>
-    <div class="background"></div>
-    <div class="wrapper">
-        <div class="outer">
-            <div class="inner">
-                <div class="content">
+    <div class="loading_overlay_background"></div>
+    <div class="loading_overlay_wrapper">
+        <div class="loading_overlay_outer">
+            <div class="loading_overlay_inner">
+                <div class="loading_overlay_content">
                     <img alt="Mr Beam Logo" height="120" width="693" src="/plugin/mrbeam/static/img/mr-typo_x120.png">
-                    <p id="message" class="text red pulsate3">beamOS is starting...</p>
+                    <p id="loading_overlay_message" class="loading_overlay_text loading_overlay_red loading_overlay_pulsate3" data-bind="text: display_state">beamOS is starting...</p>
                 </div>
             </div>
         </div>

octoprint_mrbeam/templates/mrbeam_ui_index.jinja2
@@ -35,7 +35,7 @@ for(h=0;h<k.length;h++)e(d,k[h]);a._i.push([b,c,f])};a.__SV=1.2;b=e.createElemen
 mixpanel.init("094a94a7a2bd139aa838a1b326d58529",{debug:true});</script><!-- end Mixpanel -->
 		{% endif %}
     </head>
-    <body>
+    <body style="overflow: hidden;">
 		<div class="container-fluid octoprint-container">
 		<nav class="navbar navbar-default navbar-fixed-top" role="navigation" id="navbar" style="min-width:730px;">
 			<div class="">

octoprint_mrbeam/templates/settings/camera_settings.jinja2
@@ -1,7 +1,7 @@
 
-<div id="settings_camera_calibration" style="padding-bottom:2em;">
+<div id="settings_camera_calibration" class="scrollable" style="padding-bottom:2em; overflow-y: auto; height: calc(100vh - 100px);">
 
-	<div id="calibration_step_1" class="calibration_step active" data-bind="visible: !isInitialCalibration()">
+	<div id="calibration_step_1" class="calibration_step active scrollable" data-bind="visible: !isInitialCalibration()">
 		<div class='calibration_manual img' style='background-image:url(/plugin/mrbeam/static/img/cam_calibration/calpic_material_size.svg)'></div>
 		<div class='calibration_manual text'>{{_('Please put a material like cardboard inside your Mr Beam II that it covers the whole working area.')}}</div>
 		<div class='calibration_manual img' style='background-image:url(/plugin/mrbeam/static/img/cam_calibration/calpic_close_lid.svg)'></div>
@@ -11,7 +11,7 @@
 			<button class="btn btn-default" data-bind="click: next">{{ _('Continue') }}</button>
 		</div>
 	</div>
-	<div id="calibration_step_2" class="calibration_step" data-bind="visible: !isInitialCalibration()">
+	<div id="calibration_step_2" class="calibration_step scrollable" data-bind="visible: !isInitialCalibration()" style="overflow-y: auto; height: calc(100vh - 100px);">
 		<div class='calibration_manual img' style='background-image:url(/plugin/mrbeam/static/img/cam_calibration/calpic_open_lid.svg)'></div>
 		<div class='calibration_manual text'>{{_('Wait until the engraving is finished and Mr Beam II illuminates green.\
 						Open the safety lid again')}}</div>

commit 5d2896e71c7a6026746afa53e9eb5d61854fcbd1

Solves bug that has been introduced with 9ed9a2b2 Reload overlay

octoprint_mrbeam/static/js/loadingoverlay_viewmodel.js
@@ -8,8 +8,12 @@ $(function() {
 
         self.display_state = ko.observable($('#loading_overlay_message').text());
 
-        window.onbeforeunload = function(){
-            self.showReloading();
+        window.onbeforeunload = function(event){
+            // do not show reloadingOverlay when it's a file download
+            if (!event.target.activeElement.href) {
+                console.log("Display reload overlay.");
+                self.showReloading();
+            }
         };
 
         self.removeLoadingOverlay = function(){