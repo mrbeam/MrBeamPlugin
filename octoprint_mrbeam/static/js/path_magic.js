b49b13e10387d6bd9060b2ba3b71567b0a2aa5ec
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2018-03-02 10:44:32 +0100

    Set isClosed for each path

--- octoprint_mrbeam/static/js/path_magic.js
+++ octoprint_mrbeam/static/js/path_magic.js
@@ -721,7 +721,19 @@ var mrbeam = mrbeam || {};
 
     var solution = new ClipperLib.PolyTree();
     var c = new ClipperLib.Clipper();
-    c.AddPaths(subj, ClipperLib.PolyType.ptSubject, false);
+
+    subj.forEach(path => {
+      if (path.length == 0)
+          return
+  
+      var startPoint = path[0];
+      var endPoint = path[path.length - 1];
+
+      var isClosed = (startPoint.X == endPoint.X) && (startPoint.Y == endPoint.Y);
+
+      c.AddPath(path, ClipperLib.PolyType.ptSubject, isClosed);
+    });
+
     c.AddPaths(clip, ClipperLib.PolyType.ptClip, true);
     c.Execute(
       ClipperLib.ClipType.ctIntersection,
