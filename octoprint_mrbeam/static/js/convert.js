cb3ee92eebef12cc65224a5e942422d82f4db9ac
Iratxe Espartza Chueca <iratxe@mr-beam.org>
2018-11-05 16:26:53 +0100

    Add tooltips to show info about parameters

--- octoprint_mrbeam/static/js/convert.js
+++ octoprint_mrbeam/static/js/convert.js
@@ -1311,6 +1311,10 @@ $(function(){
                 material_burger_menu
                 $('#material_burger_menu').hide()
             }
+
+            $('[data-toggle="tooltip"]').tooltip({
+                html:true
+            });
 		};
 
 		self.onSlicingProgress = function(slicer, model_path, machinecode_path, progress){
@@ -1528,7 +1532,7 @@ window.mrbeam.colorDragging = {
 
 		var data = ev.dataTransfer.getData("text");
 		if(data !== 'cd_engraving'){
-			var newJob = $('#first_job').clone(true);
+			var newJob = $('#first_job').clone(); //clone(true) --> https://github.com/twbs/bootstrap/issues/18326
 			newJob.attr('id', '');
 			var i = $('.job_row_vector').length + 1;
 			$(newJob).find('.job_title').text("Cutting Job " + i);
@@ -1540,6 +1544,11 @@ window.mrbeam.colorDragging = {
 			$(newJob).find('.assigned_colors').append(color);
 			ko.dataFor(document.getElementById("dialog_vector_graphics_conversion"))._update_color_assignments();
 		}
+
+        $('[data-toggle="tooltip"]').tooltip({
+            html:true
+        });
+
     },
 
 
