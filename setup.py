ddbaa5f3a76b6d891ef03eca12aede1435fca31b
Andy Werner <andy@mr-beam.org>
2019-02-12 13:23:58 +0100

    v0.1.55: mount_manager v 1.6.0 (https://bitbucket.org/mrbeam/usb_mount_manager/commits/4fcae8c6dacd26e7e44b0760a1d73ca204b5e3ec)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_mrbeam"
 plugin_name = "Mr_Beam"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.54"
+plugin_version = "0.1.55"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
