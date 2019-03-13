8f5e5682c18df6060a9b64b9ca324c064e08cf9b
Andy Werner <Gallore@users.noreply.github.com>
2019-03-13 18:41:36 +0100

    starting the laser with M3S0 to avoid firing up delays. (#419)
    * starting the laser with M3S0 to avoid firing up delays. solution uses
    200msec pierce time after moving into position before M3S0. Avoids
    confusion because no laserdot is visible and is consistent for vector
    and raster mode.
    
    * syntax error, shorter initialize waiting time (0.2s -> 0.1s)
    
    * zero waiting in G4 is enough to initialize the laser.

--- octoprint_mrbeam/gcodegenerator/machine_settings.py
+++ octoprint_mrbeam/gcodegenerator/machine_settings.py
@@ -1,13 +1,11 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+
 
 
 def gcode_before_path(intensity = 0):
-	return "\nM03 S"+str(intensity)
+	return "\nM3S0\nG4P0\nM03 S"+str(intensity)
 
 def gcode_before_path_color(color = '#000000', intensity = '0'):
-	return "\nM03 S%s;%s" % (intensity, color) 
+	return "\nM3S0\nG4P0\nM03 S%s;%s" % (intensity, color) 
 
 def gcode_after_path():
 	return "M05"
@@ -25,14 +23,15 @@ M5
 
 gcode_header = """
 $H
-G92 X0 Y0 Z0
+G92 X0 Y0 Z0 
 G90
 M08
 """
 
+# TODO remove this or fetch machine settings from settings.
 gcode_footer = """
 M05
-G0 X500.000 Y400.000
+G0 X500.000 Y390.000
 M09
 M02
 """
