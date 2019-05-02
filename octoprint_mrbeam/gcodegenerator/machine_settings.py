bccb2b71683b658c1a2ad3a6d4b8a2dc6323b084
Teja <teja@mr-beam.org>
2019-05-02 12:50:49 +0200

    give the laser head fan some spinup time at the beginning of a job.

--- octoprint_mrbeam/gcodegenerator/machine_settings.py
+++ octoprint_mrbeam/gcodegenerator/machine_settings.py
@@ -12,6 +12,17 @@ def gcode_before_path_color(color = '#000000', intensity = '0'):
 def gcode_after_path():
 	return "M05"
 
+
+
+# TODO remove this or fetch machine settings from settings. (G92 X0 Y0 Z0 looks badly wrong for Mr Beam II machines.)
+cooling_fan_speedup_gcode = """
+; speedup cooling fan
+M3S0
+G4P0.5
+M5
+; end speedup cooling fan
+"""
+
 gcode_header = """
 $H
 G92 X0 Y0 Z0
