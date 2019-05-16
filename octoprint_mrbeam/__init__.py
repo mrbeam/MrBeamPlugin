ab2b80aac1954bebff7dfc8eb543f5bb70404965
irlaec <iratxe@mr-beam.org>
2019-05-16 10:44:21 +0200

    Change Air Filter tab name to Exhaust System (#485)

--- octoprint_mrbeam/__init__.py
+++ octoprint_mrbeam/__init__.py
@@ -513,7 +513,7 @@ class MrBeamPlugin(octoprint.plugin.SettingsPlugin,
             dict(type='settings', name=gettext("About This Mr Beam"), template='settings/about_settings.jinja2', suffix="_about", custom_bindings=False),
             dict(type='settings', name=gettext("Analytics"), template='settings/analytics_settings.jinja2', suffix="_analytics", custom_bindings=False),
 			dict(type='settings', name=gettext("Reminders"), template='settings/reminders_settings.jinja2', suffix="_reminders", custom_bindings=False),
-			dict(type='settings', name=gettext("Air Filter"), template='settings/air_filter_settings.jinja2', suffix="_airfilter", custom_bindings=True),
+			dict(type='settings', name=gettext("Exhaust System"), template='settings/air_filter_settings.jinja2', suffix="_airfilter", custom_bindings=True),
       
 			# disabled in appearance
 			# dict(type='settings', name="Serial Connection DEV", template='settings/serialconnection_settings.jinja2', suffix='_serialconnection', custom_bindings=False, replaces='serial')
