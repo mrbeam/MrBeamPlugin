c9082813d6771f8e7abb23c33a87cff4478010eb
Iratxe Espartza Chueca <iratxe@mr-beam.org>
2019-07-09 13:05:29 +0200

    First draft

--- octoprint_mrbeam/static/js/wizard_whatsnew.js
+++ octoprint_mrbeam/static/js/wizard_whatsnew.js
@@ -1,50 +1,71 @@
 $(function() {
     function WizardWhatsnewViewModel(parameters) {
-         var self = this;
-         window.mrbeam.viewModels['wizardWhatsnewViewModel'] = self;
+        var self = this;
+        window.mrbeam.viewModels['wizardWhatsnewViewModel'] = self;
 
-         self.settings = parameters[0];
-         self.softwareChannelSelector = parameters[1];
+        self.settings = parameters[0];
 
-         self.onAfterBinding = function(){
-             $('#wizard_dialog div.modal-footer button.button-finish').text(gettext("Let's go!"));
-             $('#wizard_dialog div.modal-footer div.text-center').hide();
-             if (self.is_bound()) {
+        self.uuid = ko.observable(null);
+        self.registered = ko.observable(null);
+        self.ping = ko.observable(false);
+        self.verified = ko.observable(false);
+        self.findMrBeamWorks = ko.computed(function(){
+            return self.registered() && (self.ping() || self.verified())
+        });
+
+        self.onAfterBinding = function(){
+            $('#wizard_dialog div.modal-footer button.button-finish').text(gettext("Let's go!"));
+            $('#wizard_dialog div.modal-footer div.text-center').hide();
+            if (self.is_bound()) {
                 // test if bound, only then it's a what's new wizard
-                 $('#wizard_dialog div.modal-header h3').text("✨ " + gettext("What's New") + " ✨");
-             } else{
-                 // welcome wizard
-             }
-         };
-         
-         self.onWizardFinish = function(){
-            let selected_channel = $('#whatsnew_software_channel_select').val();
-            self.softwareChannelSelector.setChannelAsync(selected_channel, 1500);
-            // selected_channel is undefined if whatsnew screen isn't show but welcome wizard is.
-            if (selected_channel != undefined && selected_channel != self.settings.settings.plugins.mrbeam.dev.software_tier()) {
-                new PNotify({
-                        title: gettext("Switching Software Channel"),
-                        text: gettext("This takes a fiew seconds..."),
-                        hide: true,
-                        delay: 20 * 1000,
-                        icon: "icon-cog icon-spin",
-                    });
+                $('#wizard_dialog div.modal-header h3').text("✨ " + gettext("What's New") + " ✨");
+            } else{
+                // welcome wizard
             }
         };
 
-         self.is_bound = function(){
-            var elem = document.getElementById(DOM_ELEMENT_TO_BIND_TO);
-            return elem ? (!!ko.dataFor(elem)) : false;
-         };
+        self.onStartupComplete = function () {
+            self.verifyByFrontend();
+        };
 
+        self.onAllBound = function () {
+            self.uuid(self.settings.settings.plugins.findmymrbeam.uuid());
+            self.registered(self.settings.settings.plugins.findmymrbeam.registered());
+            self.ping(self.settings.settings.plugins.findmymrbeam.ping());
+        };
 
+        self.verifyByFrontend = function() {
+            if (self.registered()) {
+                let registryUrl = "http://find.mr-beam.org/verify";
+                let requestData = {
+                    uuid: self.uuid(),
+                    frontendHost: document.location.host
+                };
+                $.get(registryUrl, requestData)
+                    .done(function (response) {
+                        self.verified(response['verified'] || false);
+                        self.verification_response = response
+                    })
+                    .fail(function () {
+                        self.verified(false);
+                        self.verification_response = null;
 
+                    })
+            } else {
+                self.verified(false);
+            }
+        };
+
+        self.is_bound = function(){
+           var elem = document.getElementById(DOM_ELEMENT_TO_BIND_TO);
+           return elem ? (!!ko.dataFor(elem)) : false;
+        };
     }
 
     var DOM_ELEMENT_TO_BIND_TO = "wizard_plugin_corewizard_whatsnew_0";
     OCTOPRINT_VIEWMODELS.push([
         WizardWhatsnewViewModel,
-        ['settingsViewModel', 'softwareChannelSelector'],
+        ['settingsViewModel'],
         "#"+DOM_ELEMENT_TO_BIND_TO
     ]);
 });
