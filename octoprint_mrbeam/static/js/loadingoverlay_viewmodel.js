ad493fb2cffdce991cba5cb9b932f3508c788f3a
Andy Werner <andy@mr-beam.org>
2019-07-19 11:36:03 +0200

    Fixed Freshdesk Widged blocking DOM rendering when offline

--- octoprint_mrbeam/static/js/loadingoverlay_viewmodel.js
+++ octoprint_mrbeam/static/js/loadingoverlay_viewmodel.js
@@ -3,23 +3,32 @@
 $(function() {
     function LoadingOverlayViewModel(parameters) {
         var self = this;
+        window.mrbeam.viewModels['loadingOverlayViewModel'] = self;
+
+        self.allViewModels = null;
 
         self.TEXT_RELOADING = gettext("beamOS is reloading...");
 
         self.display_state = ko.observable($('#loading_overlay_message').text());
+
+        self.onAllBound = function(allViewModels){
+            self.allViewModels = allViewModels;
+        };
 
         $( window ).on('beforeunload', function(){
             // do not show reloadingOverlay when it's a file download
             if (!event.target.activeElement.href) {
                 console.log("Display reload overlay.");
                 self.showReloading();
+                callViewModels(self.allViewModels, 'onCurtainClosed');
             }
         });
 
         self.removeLoadingOverlay = function(){
             $('#loading_overlay').hide();
+            callViewModels(self.allViewModels, 'onCurtainOpened');
             console.log("beamOS started. overlay removed.");
-            console.log("%c      ", "color: transparent; font-size: 150px; background:url('http://www.mr-beam.org/img/logo2_path.svg') no-repeat bottom left");
+            console.log("%c      ", "color: transparent; font-size: 150px; background:url('https://www.mr-beam.org/wp-content/themes/mrbeam/mysite/images/logo-icon.svg') no-repeat bottom left");
         };
 
         self.showReloading = function(){
