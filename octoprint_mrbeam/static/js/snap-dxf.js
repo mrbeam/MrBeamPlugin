258805c7bafbd019a15b7ba771b3431496577633
Teja <teja@mr-beam.org>
2019-10-07 18:36:07 +0200

    dxf.js 4.2.2 joined svg paths (#585)
    * first prototype of joining paths during dxf import.
    
    * updated dxf.js to 4.2.2, implemented joined path feature.

--- octoprint_mrbeam/static/js/snap-dxf.js
+++ octoprint_mrbeam/static/js/snap-dxf.js
@@ -4,22 +4,24 @@
 //    dxf = require('./dxf.js');
 
 Snap.plugin(function (Snap, Element, Paper, global, Fragment) {
-    Snap.parseDXF = function (dxfString) {
-        var convertedSVG = dxf.toSVG(dxf.parseString(dxfString));
-        return Snap.parse(convertedSVG);
-    };
+	Snap.parseDXF = function (dxfString) {
+		var parsed = dxf.parseString(dxfString);
+//		var convertedSVG = dxf.toSVG(parsed);
+		var convertedSVG = dxf.toSVGPaths(parsed);
+		return Snap.parse(convertedSVG);
+	};
 
-    Snap.loadDXF = function (url, callback, scope) {
-        Snap.ajax(url, function (req) {
-            var timestamps = {
-                load_done: Date.now(),
-                parse_start: Date.now(),
-                parse_done: -1,
-            };
-            var f = Snap.parseDXF(req.responseText);
-            timestamps.parse_done = Date.now();
-            if (callback)
-                scope ? callback.call(scope, f, timestamps) : callback(f, timestamps);
-        });
-    };
+	Snap.loadDXF = function (url, callback, scope) {
+		Snap.ajax(url, function (req) {
+			var timestamps = {
+				load_done: Date.now(),
+				parse_start: Date.now(),
+				parse_done: -1,
+			};
+			var f = Snap.parseDXF(req.responseText);
+			timestamps.parse_done = Date.now();
+			if (callback)
+				scope ? callback.call(scope, f, timestamps) : callback(f, timestamps);
+		});
+	};
 });
