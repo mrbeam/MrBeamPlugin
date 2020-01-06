9b2175eafa91d94689237454fe3b7919a2f13878
Teja <teja@mr-beam.org>
2020-01-06 11:27:57 +0100

    bugfix: check if coordinates present before setting them via knockout/jquery

--- octoprint_mrbeam/static/js/mother_viewmodel.js
+++ octoprint_mrbeam/static/js/mother_viewmodel.js
@@ -408,7 +408,7 @@ $(function () {
 		};
 
         self._processWPosData = function (data) {
-            if (data === undefined || data === null) {
+            if (data === undefined || data === null || data[0] === null || data[1] === null || isNaN(data[0]) || isNaN(data[1])) {
                 self.state.currentPos({x: 0, y: 0});
             } else {
                 self.state.currentPos({x: data[0], y: data[1]});
