16fc761a0201e6e86c5b5fa79404fbdc2b9ec39d
Axel Soll <axel@soll.xyz>
2020-01-28 14:15:35 +0100

    Do not install picamra on x86 machines

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/mrbeam/MrBeamPlugin"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ['webcolors', 'pillow', 'lxml', 'numpy==1.11.2', 'picamera']
+plugin_requires = ['webcolors', 'pillow', 'lxml', 'numpy==1.11.2']
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
@@ -63,7 +63,7 @@ additional_setup_parameters = {'package_data': {
 									                     'files/migrate/*',
 									                     'files/migrate_logrotate/*',
 														 'files/material_settings/*']},
-									'setup_requires': ['numpy==1.11.2']}
+									'setup_requires': ['numpy==1.11.2', 'picamera; platform_machine=="armv7l"']}
 
 ########################################################################################################################
 
