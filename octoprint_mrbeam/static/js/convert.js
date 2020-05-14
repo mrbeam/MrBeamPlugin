31df8058442e0589f9c2c9a7477374e0f8b90f1c
Iratxe Espartza Chueca <iratxe@mr-beam.org>
2020-05-14 10:46:35 +0200

    Sliders: change Mr Beam lights + refactoring

--- octoprint_mrbeam/static/js/convert.js
+++ octoprint_mrbeam/static/js/convert.js
@@ -1656,7 +1656,7 @@ $(function(){
 
                         let outer = '<div id="' + slider_id + '_out"></div>';
                         let color_circle = '<div class="vector_mapping_color_circle" style="background:' + hex + '"/></div>';
-                        let slider = '<input id="'+slider_id+'" class="svgtogcode_grayscale conversion_range_slider vector_mapping_slider" type="range" min="0" max="255" value="'+val+'" /></div>';
+                        let slider = '<input id="'+slider_id+'" class="svgtogcode_grayscale mrb_slider conversion_range_slider" type="range" min="0" max="255" value="'+val+'" /></div>';
 
                         line_mapping_container.append(outer);
                         $('#' + slider_id + '_out').append(color_circle);
