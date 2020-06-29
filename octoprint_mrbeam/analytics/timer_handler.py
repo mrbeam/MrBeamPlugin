5f77d18ffcace8164724a933feda326667355d72
Axel Soll <axel@soll.xyz>
2020-06-29 17:46:55 +0200

    rm unnecessary logs.

--- octoprint_mrbeam/analytics/timer_handler.py
+++ octoprint_mrbeam/analytics/timer_handler.py
@@ -43,10 +43,7 @@ class TimerHandler:
 			self._timers.append(Timer(self.INTERNET_CONNECTION_TIMER, self._internet_connection))
 			if not (self._plugin._settings.get(['dev', 'support_mode']) or \
 			        self._plugin.calibration_tool_mode):
-				self._logger.warning("do the update you silly willy")
 				self._timers.append(Timer(self.SW_AND_CHECKSUMS_TIMER, self._software_versions_and_checksums))
-			else:
-				self._logger.warning("DON'T do the update you silly willy")
 			self._timers.append(Timer(self.FILE_CROP_TIMER, self._crop_analytics_file_if_too_big))
 
 			for timer in self._timers:
