1fa27710b0f4a6346f321b7a32d873fb55cc78a9
Axel Soll <axel@soll.xyz>
2020-10-19 15:56:54 +0200

    SW-1350 Analysis Queue refactor

--- octoprint_mrbeam/__init__.py
+++ octoprint_mrbeam/__init__.py
@@ -3139,7 +3139,7 @@ def __plugin_load__():
         "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
         "octoprint.printer.factory": __plugin_implementation__.laser_factory,
         "octoprint.filemanager.extension_tree": __plugin_implementation__.laser_filemanager,
-        "octoprint.filemanager.analysis.factory": beam_analysis_queue_factory,  # Only used in OP v1.3.11 +
+        "octoprint.filemanager.analysis.factory": beam_analysis_queue_factory,
         "octoprint.server.http.bodysize": __plugin_implementation__.bodysize_hook,
         "octoprint.cli.commands": get_cli_commands,
     }
