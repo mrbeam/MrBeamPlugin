0a9aa5d6774e553fa83e5dc4a078ccf07efc0059
khaledsherkawi <54568489+khaledsherkawi@users.noreply.github.com>
2021-07-14 16:29:53 +0200

    Design store notify new designs (#1296)
    * last uploaded date setting added
    
    * design store link fixed
    
    * last uploaded date updated on design store click
    
    * Update octoprint_mrbeam/static/js/design_store.js
    
    Co-authored-by: Andy Werner <Gallore@users.noreply.github.com>
    
    * styling fix for switching between design store environments
    
    * don't show notification if uploaded value is undefined in users.yaml
    
    Co-authored-by: Andy Werner <Gallore@users.noreply.github.com>

--- octoprint_mrbeam/static/js/design_store.js
+++ octoprint_mrbeam/static/js/design_store.js
@@ -134,7 +134,7 @@ $(function () {
 
         self.onLastUploadedDateReceived = function (payload) {
             let oldLastUploaded = self.getLastUploadedDate();
-            if (payload.last_uploaded && oldLastUploaded !== payload.last_uploaded) {
+            if (payload.last_uploaded && oldLastUploaded && oldLastUploaded !== payload.last_uploaded && $("#designstore_tab_btn span.red-dot").length === 0) {
                 // Notify user
                 $("#designstore_tab_btn").append('<span class="red-dot"></span>');
             }
@@ -226,7 +226,7 @@ $(function () {
                 self.sendMessageToDesignStoreIframe("goToStore", {});
             }
             let oldLastUploaded = self.getLastUploadedDate();
-            if (self.lastUploadedDate() !== "" && oldLastUploaded !== self.lastUploadedDate()) {
+            if (self.lastUploadedDate() && self.lastUploadedDate() !== "" && oldLastUploaded !== self.lastUploadedDate()) {
                 self.saveLastUploadedInUserSettings(self.lastUploadedDate());
             }
         };
