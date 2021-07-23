b754c787cbb962eefbe889acc3dd6a1cf0c8b659
khaledsherkawi <khaledshirkawy@gmail.com>
2021-07-23 09:10:46 +0200

    active class added only to first element

--- octoprint_mrbeam/static/js/settings_menu_navigation.js
+++ octoprint_mrbeam/static/js/settings_menu_navigation.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     let settingsElement = $("#settings_dialog #settings_dialog_menu ul li");
-    // let settingsActiveElement = $("#settings_dialog #settings_dialog_menu ul li.active");
     settingsElement.hover(
         function () {
             $(this).prev().addClass("prev");
