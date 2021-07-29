c0676efd3cd44b595f72c8de58fbbb81fb39bac4
Sherif Hussien <sherifhussien1996@gmail.com>
2021-07-29 16:48:29 +0200

    finalized

--- tests/frontend/users/test_user.py
+++ tests/frontend/users/test_user.py
@@ -3,6 +3,9 @@ import octoprint.users
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 from frontend.users.base_procedure import BaseProcedure
 
@@ -34,6 +37,7 @@ class TestUser(BaseProcedure):
     @pytest.mark.usefixtures('disable_firstrun', as_attrs=False)
     def test_login_user(self, username, password):
 
+        # login
         self.driver.find_element(By.ID, 'login_screen_email_address_in').send_keys(username)
         self.driver.find_element(By.ID, 'login_screen_password_in').send_keys(password)
 
@@ -42,6 +46,25 @@ class TestUser(BaseProcedure):
         assert self.driver.execute_script(js) == False
         self.driver.find_element(By.ID, 'login_screen_login_btn').click()
         assert self.driver.execute_script(js) == True
+
+        # safety wizard
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, 'next'))).click()
+
+        print(self.driver.find_elements(By.XPATH, "//li[@id='wizard_plugin_corewizard_lasersafety']//input"))
+        for checkbox in self.driver.find_elements(By.XPATH, "//div[@id='wizard_plugin_corewizard_lasersafety']//input"):
+            checkbox.click()
+
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, 'next'))).click()
+
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, 'finish'))).click()
+
+        # logout
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='icon-remove']"))).click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//li[@id='navbar_login']/a"))).click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@id='logout_button']"))).click()
+
+        assert self.driver.execute_script(js) == False
+
 
 
 
