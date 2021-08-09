e32945b2b6ec717485ff8a756e01cea6bb8f6ff1
Sherif Hussien <sherifhussien1996@gmail.com>
2021-08-09 12:56:19 +0200

    update

--- tests/frontend/users/test_user.py
+++ tests/frontend/users/test_user.py
@@ -16,7 +16,7 @@ class TestUser(BaseProcedure):
     @pytest.mark.parametrize("username, password", [
         ('sherif@gmail.com', 'secret'),
     ])
-    @pytest.mark.usefixtures('enable_firstrun', as_attrs=False)
+    @pytest.mark.usefixtures('enable_firstrun', as_attrs=True)
     def test_add_user(self, username, password):
 
         self.driver.find_element(By.NAME, 'next').click()
@@ -37,31 +37,34 @@ class TestUser(BaseProcedure):
     @pytest.mark.usefixtures('disable_firstrun', as_attrs=False)
     def test_login_user(self, username, password):
 
+        js = 'return mrbeam.viewModels.loginScreenViewModel.loginState.loggedIn();'
+        assert self.driver.execute_script(js) == False
+
         # login
         self.driver.find_element(By.ID, 'login_screen_email_address_in').send_keys(username)
         self.driver.find_element(By.ID, 'login_screen_password_in').send_keys(password)
-
-        js = 'return mrbeam.viewModels.loginScreenViewModel.loginState.loggedIn();'
-
-        assert self.driver.execute_script(js) == False
         self.driver.find_element(By.ID, 'login_screen_login_btn').click()
+
         assert self.driver.execute_script(js) == True
 
         # safety wizard
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, 'next'))).click()
 
-        print(self.driver.find_elements(By.XPATH, "//li[@id='wizard_plugin_corewizard_lasersafety']//input"))
         for checkbox in self.driver.find_elements(By.XPATH, "//div[@id='wizard_plugin_corewizard_lasersafety']//input"):
             checkbox.click()
 
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, 'next'))).click()
-
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, 'finish'))).click()
 
+        # hide all alerts
+        print(self.driver.find_elements(By.XPATH, "//div[@class='alert']"))
+
+        for alert in self.driver.find_elements(By.XPATH, "//div[@role='alert']"):
+            self.driver.execute_script("arguments[0].style.display = 'none';", alert)
+
         # logout
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='icon-remove']"))).click()
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//li[@id='navbar_login']/a"))).click()
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@id='logout_button']"))).click()
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//a[@id='logout_button']"))).click()
 
         assert self.driver.execute_script(js) == False
 
