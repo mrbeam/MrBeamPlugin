9be6f24c5b846825ac7adb4e8a5476245ba75327
khaledsherkawi <khaledshirkawy@gmail.com>
2021-08-20 13:48:27 +0200

    Add DC S model to devices that has compressor

--- octoprint_mrbeam/iobeam/compressor_handler.py
+++ octoprint_mrbeam/iobeam/compressor_handler.py
@@ -85,7 +85,7 @@ class CompressorHandler(object):
         )
 
     def has_compressor(self):
-        return self._plugin.is_mrbeam2_dreamcut()
+        return self._plugin.is_mrbeam2_dreamcut() or self._plugin.is_mrbeam2_dreamcut_s()
 
     def get_current_state(self):
         if self.has_compressor():
