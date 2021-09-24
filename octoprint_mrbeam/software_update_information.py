4ecfcc2d49835023389970d3f4c66bdd4f00d197
alexloss <axel@soll.xyz>
2021-09-24 16:14:09 +0200

    Add debug log message for update targets.

--- octoprint_mrbeam/software_update_information.py
+++ octoprint_mrbeam/software_update_information.py
@@ -47,7 +47,7 @@ def get_update_information(plugin):
     # The increased number of separate virtualenv for iobeam, netconnectd, ledstrips
     # will increase the "discovery time" to find those package versions.
     # "map-reduce" method can decrease lookup time by processing them in parallel
-    return dict(
+    res = dict(
         reduce(
             dict_merge,
             [
@@ -63,6 +63,15 @@ def get_update_information(plugin):
             ],
         )
     )
+    for pack, updt_info in res.items():
+        _logger.debug(
+            "{} targets branch {} using pip {}".format(
+                pack,
+                updt_info.get("branch"),
+                updt_info.get("pip_command", "~/oprint/bin/pip"),
+            )
+        )
+    return res
 
 
 def software_channels_available(plugin):
