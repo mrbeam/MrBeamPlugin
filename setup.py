25661e14bebb3b235ff5c8e4dd9e750ae4269eed
Josef-MrBeam <josef@mr-beam.org>
2022-02-09 13:45:19 +0100

    dont't fix versionnumber

--- setup.py
+++ setup.py
@@ -44,12 +44,12 @@ if LooseVersion(setuptools.__version__) < LooseVersion("40.0"):
 else:
     picamera = 'picamera; platform_machine=="armv7l"'
 plugin_requires = [
-    "webcolors == 1.10",
-    "pillow == 6.2.2",
-    "lxml == 4.6.3",
-    "numpy == 1.16.6",
-    "pyyaml == 3.10",
-    "enum34 == 1.1.10",
+    "webcolors <= 1.10",
+    "pillow <= 6.2.2",
+    "lxml <= 4.7.1",
+    "numpy <= 1.16.6",
+    "pyyaml <= 3.10",
+    "enum34 <= 1.1.10",
     picamera,
 ]
 
