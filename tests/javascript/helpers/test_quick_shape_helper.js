ee65cddf6a6c9f028323b951f294efbc28a9b16d
Yelko <yelko@mr-beam.org>
2022-02-09 10:24:06 +0100

    SW-990 add example unit tests

---
+++ tests/javascript/helpers/test_quick_shape_helper.js
@@ -0,0 +1,18 @@
+var assert = chai.assert;
+
+describe('QuickShapeHelper test', function () {
+    it('Radio 0 should return empty string', function () {
+        var arr = [];
+
+        let circle = QuickShapeHelper.getCircle(0);
+        assert.isEmpty(circle)
+    });
+
+    it('Radio -1 should return empty string', function () {
+        var arr = [];
+
+        let circle = QuickShapeHelper.getCircle(-1);
+        console.log(circle)
+        assert.isEmpty(circle)
+    });
+});
