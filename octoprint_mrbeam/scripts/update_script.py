5af2576758920b70bb21a017392689780a744a0d
Josef-MrBeam <josef@mr-beam.org>
2022-03-31 11:12:38 +0200

    change regex of dependency versions to match PEP-440

--- octoprint_mrbeam/scripts/update_script.py
+++ octoprint_mrbeam/scripts/update_script.py
@@ -118,7 +118,7 @@ def get_dependencies(path):
         list of dependencie dict [{"name", "version"}]
     """
     dependencies_path = os.path.join(path, "dependencies.txt")
-    dependencies_pattern = r"([a-z]+(?:[_-][a-z]+)*)(.=)+((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)"
+    dependencies_pattern = r"([a-z]+(?:[_-][a-z]+)*)(.=)+(([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?)"
     """
     Example:
     input:  iobeam==0.7.15
