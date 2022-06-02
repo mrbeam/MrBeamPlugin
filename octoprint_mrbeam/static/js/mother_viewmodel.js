fb91ad93ece6869ffb750e53fe257b427e1a1670
Josef-MrBeam <81746291+Josef-MrBeam@users.noreply.github.com>
2022-06-02 16:55:31 +0200

    SW-1136-cancelling-a-laser-job-message-not-clear-enough (#1498)
    * change cancel modal to talk about a laserjob instead of a print

--- octoprint_mrbeam/static/js/mother_viewmodel.js
+++ octoprint_mrbeam/static/js/mother_viewmodel.js
@@ -52,6 +52,24 @@ $(function () {
             "Starts the laser job"
         );
 
+        // override the default octoprint cancel modal
+        self.state.cancel = function() {
+            if (!self.settings.feature_printCancelConfirmation()) {
+                OctoPrint.job.cancel();
+            } else {
+                showConfirmationDialog({
+                    message: gettext("The laserjob will be cancelled."),
+                    question: gettext("Do you still want to cancel?"),
+                    cancel: gettext("No"),
+                    proceed: gettext("Yes"),
+                    proceedClass: "primary",
+                    onproceed: function() {
+                        OctoPrint.job.cancel();
+                    }
+                });
+            }
+        };
+
         self.onStartup = function () {
             // TODO fetch machine profile on start
             //self.requestData();
