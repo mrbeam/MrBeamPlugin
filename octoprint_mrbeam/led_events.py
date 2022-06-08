5f7c5e22e22f2cb537a6ec6648be5ab93312abaa
Josef-MrBeam <josef@mr-beam.org>
2022-06-08 18:17:36 +0200

    SW-1351 fix process command see 69b0f01a1dc19403b29b5b43e0850daa976d7abe

--- octoprint_mrbeam/led_events.py
+++ octoprint_mrbeam/led_events.py
@@ -192,9 +192,9 @@ class LedEventListener(CommandTrigger):
             if isinstance(command, (tuple, list, set)):
                 processedCommand = []
                 for c in command:
-                    processedCommand.append(self._processCommand(c, payload))
+                    processedCommand.append(self._processCommand(c, event, payload))
             else:
-                processedCommand = self._processCommand(command, payload)
+                processedCommand = self._processCommand(command, event, payload)
 
             self._logger.debug("LED_EVENT %s: '%s'", event, processedCommand)
             self.executeCommand(processedCommand, commandType, debug=debug)
