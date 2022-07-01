55974f36a8c3a263964a5e9d10858d7971f75c53
khaledsherkawi <khaledshirkawy@gmail.com>
2022-07-01 07:55:15 +0200

    SW-1328 Fix MRBEAM_DEBUG_RENDERING debugging

--- octoprint_mrbeam/static/js/working_area.js
+++ octoprint_mrbeam/static/js/working_area.js
@@ -3249,11 +3249,7 @@ $(function () {
             if (MRBEAM_DEBUG_RENDERING) {
                 debugBase64(
                     clusters.map((c) => {
-                        c.svg.attr(
-                            "viewBox",
-                            `0 0 ${self.workingAreaWidthMM()} ${self.workingAreaHeightMM()}`
-                        );
-                        return c.svg.toDataURL();
+                        return c.svgDataUrl;
                     }),
                     `Step 1: Raster Cluster SVGs`
                 );
