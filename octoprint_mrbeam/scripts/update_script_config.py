855d23d7f67eb2fc7e2f5a53c7bbd28942bc278d
irlaec <irlaec@gmail.com>
2022-07-14 09:56:56 +0200

    Revert "SW-1180 split the updatescript to use params from a config file (#1522)"
    This reverts commit 1a0c0e0ab4d5f36e4397a7a3e7c9b22c0558ea42.

--- octoprint_mrbeam/scripts/update_script_config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-UPDATE_CONFIG_NAME = "mrbeam"
-REPO_NAME = "MrBeamPlugin"
-MAIN_SRC_FOLDER_NAME = "octoprint_mrbeam"
-PLUGIN_NAME = "Mr_Beam"
-DEFAULT_OPRINT_VENV = os.environ.get("DEFAULT_OPRINT_VENV", "/home/pi/oprint/bin/pip")
-PIP_WHEEL_TEMP_FOLDER = "/tmp/wheelhouse"
-IMPORT_TOP_DIR = "octoprint_mrbeam"
