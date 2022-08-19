8df26973e4074cbff8897de72649a41a6105dffc
Yelko <yelko@mr-beam.org>
2022-08-19 16:12:51 +0200

    SW-1599 run formating for json files

--- tests/javascript/app/helpers/quick-shape-test.js
+++ tests/javascript/app/helpers/quick-shape-test.js
@@ -1,18 +1,18 @@
 var assert = chai.assert;
 
-describe('QuickShapeHelper test', function () {
-    it('Radio 0 should return empty string', function () {
+describe("QuickShapeHelper test", function () {
+    it("Radio 0 should return empty string", function () {
         var arr = [];
 
         let circle = QuickShapeHelper.getCircle(0);
-        assert.isEmpty(circle)
+        assert.isEmpty(circle);
     });
 
-    it('Radio -1 should return empty string', function () {
+    it("Radio -1 should return empty string", function () {
         var arr = [];
 
         let circle = QuickShapeHelper.getCircle(-1);
-        console.log(circle)
-        assert.isEmpty(circle)
+        console.log(circle);
+        assert.isEmpty(circle);
     });
 });
