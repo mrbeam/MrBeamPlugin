69289e473029b1a74be0cc6da002dd31800bf63a
Iratxe <irlaec@gmail.com>
2022-08-22 15:42:48 +0200

    SW-1588 change ubuntu version and cypress project id (#1568)
    * Change ubuntu version in docker
    
    * Add cypress project id
    
    * Exclude cypress data from git

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+    projectId: "zpa4f8",
+    defaultCommandTimeout: 10000,
+    requestTimeout: 30000,
+    screenshotOnRunFailure: true,
+    video: true,
+    videoUploadOnPasses: false,
+    viewportHeight: 980,
+    viewportWidth: 1920,
+    chromeWebSecurity: false,
+    e2e: {
+        setupNodeEvents(on, config) {
+            return require("./cypress/plugins/index.js")(on, config);
+        },
+        baseUrl: "http://localhost:5002/",
+    },
+});
