2a50b994a08c524c39a875693b01f530add6e2d8
Josef-MrBeam <josef@mr-beam.org>
2022-09-08 09:05:56 +0200

    SW-1014 added migration step changed logrotate files migration has to be tested

---
+++ octoprint_mrbeam/migration/Mig003.py
@@ -0,0 +1,49 @@
+import os
+
+from octoprint_mrbeam.migration.migration_base import (
+    MigrationBaseClass,
+)
+
+
+class Mig003EnableLogrotateBuster(MigrationBaseClass):
+    """
+    This migration should add logrotate for the buster image and change the lorotate for the legacy image
+    """
+
+    MIGRATE_LOGROTATE_FOLDER = "files/migrate_logrotate/"
+    BEAMOS_VERSION_LOW = "0.14.0"
+    BEAMOS_VERSION_HIGH = "0.18.1"
+
+    def __init__(self, plugin):
+        super(Mig003EnableLogrotateBuster, self).__init__(plugin)
+
+    @property
+    def id(self):
+        return "003"
+
+    def _run(self):
+        self._logger.debug("delete wrong iobeam logrotate")
+        self.exec_cmd("sudo rm /etc/logrotate.d/iobeam.logrotate")
+
+        logrotates = [
+            "analytics",
+            "iobeam",
+            "mount_manager",
+            "mrb_check",
+            "mrbeam_ledstrips",
+        ]
+        for logrotate in logrotates:
+            self._logger.debug("enable logrotate of " + logrotate)
+            src = os.path.join(
+                __package_path__, self.MIGRATE_LOGROTATE_FOLDER, logrotate
+            )
+            dst = os.path.join("/etc/logrotate.d/" + logrotate)
+            self.exec_cmd("sudo cp {src} {dst}".format(src=src, dst=dst))
+
+        self._logger.debug("restart of logrotate so changed config will take affect")
+        self.exec_cmd("sudo logrotate /etc/logrotate.conf")
+        super(Mig003EnableLogrotateBuster, self)._run()
+
+    def _rollback(self):
+        # no rollback needed
+        super(Mig003EnableLogrotateBuster, self)._rollback()
