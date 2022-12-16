40fea42a585feb724a07ab4d215b62d927f149a8
Agnieszka12ax <106971121+Agnieszka12ax@users.noreply.github.com>
2022-12-16 11:39:43 +0100

    Update cypress/e2e/settings/camera.cy.js
    Co-authored-by: khaledsherkawi <54568489+khaledsherkawi@users.noreply.github.com>

--- cypress/e2e/settings/camera.cy.js
+++ cypress/e2e/settings/camera.cy.js
@@ -48,7 +48,7 @@ describe("Navbar icons", function () {
     it("Coner Calibration - start", function () {
         cy.get('[id="settings_plugin_mrbeam_camera_link"]').click();
         cy.get('[data-test="camera-settings-start-button-lens"]').click();
-        cy.get('[data-test="view-corner-calibration-svg" ').should(
+        cy.get('[data-test="view-corner-calibration-svg"]').should(
             "to.visible"
         );
         cy.get('[data-test="view-corner-start-button-back"]').click();
