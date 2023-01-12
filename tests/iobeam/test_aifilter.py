5469f5ea9ac636761b539606cdbe5a22e29ddf3f
Josef-MrBeam <josef@mr-beam.org>
2023-01-12 15:30:28 +0100

    split tests

--- tests/iobeam/test_aifilter.py
+++ tests/iobeam/test_aifilter.py
@@ -1,16 +1,21 @@
+from mock.mock import MagicMock
+
 from octoprint_mrbeam.iobeam.airfilter import AirFilter
 
 
-def test_model_name():
+def test_model_name_AF1_or_fan():
     # Arrange
-    air_filter = AirFilter(None)
+    air_filter = AirFilter(MagicMock())
     air_filter.model_id = 0
     # Act
     model_name = air_filter.model
     # Assert
     assert model_name == "Air Filter System | Fan"
 
+
+def test_model_name_AF2():
     # Arrange
+    air_filter = AirFilter(MagicMock())
     for i in range(1, 8):
         air_filter.model_id = i
         # Act
@@ -18,14 +23,20 @@ def test_model_name():
         # Assert
         assert model_name == "Air Filter II System"
 
+
+def test_model_name_AF3():
     # Arrange
+    air_filter = AirFilter(MagicMock())
     air_filter.model_id = 8
     # Act
     model_name = air_filter.model
     # Assert
     assert model_name == "Air Filter 3 System"
 
+
+def test_model_name_invalid_model_id():
     # Arrange
+    air_filter = AirFilter(MagicMock())
     air_filter.model_id = None
     # Act
     model_name = air_filter.model
@@ -42,7 +53,7 @@ def test_model_name():
 
 def test_model_id():
     # Arrange
-    air_filter = AirFilter(None)
+    air_filter = AirFilter(MagicMock())
     air_filter.model_id = 0
     # Act
     model_id = air_filter.model_id
@@ -52,7 +63,7 @@ def test_model_id():
 
 def test_serial():
     # Arrange
-    air_filter = AirFilter(None)
+    air_filter = AirFilter(MagicMock())
     air_filter.serial = "123456"
     # Act
     serial = air_filter.serial
@@ -60,16 +71,19 @@ def test_serial():
     assert serial == "123456"
 
 
-def test_pressure():
+def test_pressure_set_only_one():
     # Arrange
-    air_filter = AirFilter(None)
+    air_filter = AirFilter(MagicMock())
     air_filter.set_pressure(pressure=1)
     # Act
     pressure = air_filter.pressure
     # Assert
     assert pressure == 1
 
+
+def test_pressure_set_multiple():
     # Arrange
+    air_filter = AirFilter(MagicMock())
     air_filter.set_pressure(pressure1=1, pressure2=2, pressure3=3, pressure4=4)
     # Act
     pressure = air_filter.pressure
@@ -81,22 +95,20 @@ def test_pressure():
         "pressure4": 4,
     }
 
+
+def test_pressure_set_invalid_data():
     # Arrange
+    air_filter = AirFilter(MagicMock())
     air_filter.set_pressure(pressure2=None)
     # Act
     pressure = air_filter.pressure
     # Assert
-    assert pressure == {
-        "pressure1": 1,
-        "pressure2": 2,
-        "pressure3": 3,
-        "pressure4": 4,
-    }
+    assert pressure is None
 
 
-def test_temperatures():
+def test_temperatures_only_first():
     # Arrange
-    air_filter = AirFilter(None)
+    air_filter = AirFilter(MagicMock())
     air_filter.set_temperatures(temperature1=1.0)
     # Act
     temperatures = air_filter.temperatures
@@ -108,7 +120,10 @@ def test_temperatures():
         "temperature4": None,
     }
 
+
+def test_temperatures_all_values_at_once():
     # Arrange
+    air_filter = AirFilter(MagicMock())
     air_filter.set_temperatures(
         temperature1=1.0, temperature2=2.0, temperature3=3.0, temperature4=4.0
     )
@@ -122,38 +137,42 @@ def test_temperatures():
         "temperature4": 4.0,
     }
 
+
+def test_temperatures_with_none_value():
     # Arrange
+    air_filter = AirFilter(MagicMock())
     air_filter.set_temperatures(temperature2=None)  # temperature2 is not set
     # Act
     temperatures = air_filter.temperatures
     # Assert
-    assert temperatures == {
-        "temperature1": 1.0,
-        "temperature2": 2.0,
-        "temperature3": 3.0,
-        "temperature4": 4.0,
-    }
+    assert temperatures is None
 
+
+def test_temperatures_int_value():
     # Arrange
+    air_filter = AirFilter(MagicMock())
     air_filter.set_temperatures(temperature2=2)  # temperature2 is set
     # Act
     temperatures = air_filter.temperatures
     # Assert
     assert temperatures == {
-        "temperature1": 1.0,
+        "temperature1": None,
         "temperature2": 2,
-        "temperature3": 3.0,
-        "temperature4": 4.0,
+        "temperature3": None,
+        "temperature4": None,
     }
 
+
+def test_temperatures_string_value():
     # Arrange
+    air_filter = AirFilter(MagicMock())
     air_filter.set_temperatures(temperature2="error")
     # Act
     temperatures = air_filter.temperatures
     # Assert
     assert temperatures == {
-        "temperature1": 1.0,
+        "temperature1": None,
         "temperature2": "error",
-        "temperature3": 3.0,
-        "temperature4": 4.0,
+        "temperature3": None,
+        "temperature4": None,
     }
