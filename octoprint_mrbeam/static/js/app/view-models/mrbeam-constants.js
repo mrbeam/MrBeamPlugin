bca2fc41728885e6ae1a690f99da9a814e224421
Josef-MrBeam <josef@mr-beam.org>
2023-02-08 14:37:19 +0100

    resolve merge

--- octoprint_mrbeam/static/js/app/view-models/mrbeam-constants.js
+++ octoprint_mrbeam/static/js/app/view-models/mrbeam-constants.js
@@ -1,4 +1,4 @@
 const MRBEAM = {
     PLUGIN_IDENTIFIER: "mrbeam", // Octoprint Plugin identifier
-    STATE_KEY: "mrb_state" // Key of the Mr Beam state array
-}
+    STATE_KEY: "mrb_state", // Key of the Mr Beam state array
+};
