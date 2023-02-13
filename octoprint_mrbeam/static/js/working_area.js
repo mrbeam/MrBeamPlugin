3d6318c881312022780190446156b8a821726df0
khaledsherkawi <khaled@mr-beam.org>
2023-02-13 16:02:29 +0100

    Revert "SW-1884 Change the quicktext select options into radio buttons (#1616)"
    This reverts commit 0763844644a60e7190fea551cb257b9d106292aa.

--- octoprint_mrbeam/static/js/working_area.js
+++ octoprint_mrbeam/static/js/working_area.js
@@ -3761,18 +3761,17 @@ $(function () {
             }
             self._qt_currentQuickTextUpdate();
         });
-
-        let quickTextRadioInput = $('input[type=radio][name=stroke_or_fill]');
-        quickTextRadioInput.on("change", function () {
-            if(self.currentQuickTextFile){
-                if (this.value === "stroke") {
-                        self.currentQuickTextFile.stroke = self.lastQuickTextStroke = true;
-                        self.currentQuickTextFile.fill = self.lastQuickTextFill = false;
-                }
-                if (this.value === "fill"){
-                        self.currentQuickTextFile.fill = self.lastQuickTextFill = true;
-                        self.currentQuickTextFile.stroke = self.lastQuickTextStroke = false;
-                }
+        $("#quick_text_stroke").on("click", function (e) {
+            if (self.currentQuickTextFile) {
+                self.currentQuickTextFile.stroke = e.currentTarget.checked;
+                self.lastQuickTextStroke = self.currentQuickTextFile.stroke;
+            }
+            self._qt_currentQuickTextUpdate();
+        });
+        $("#quick_text_fill").on("click", function (e) {
+            if (self.currentQuickTextFile) {
+                self.currentQuickTextFile.fill = e.currentTarget.checked;
+                self.lastQuickTextFill = self.currentQuickTextFile.fill;
             }
             self._qt_currentQuickTextUpdate();
         });
@@ -3853,11 +3852,10 @@ $(function () {
         self._qt_currentQuickTextUpdate = function () {
             if (self.currentQuickTextFile) {
                 self.currentQuickText(self.currentQuickTextFile.name);
-                let quickTextInputField = $("#quick_text_dialog_text_input");
                 const displayText =
                     self.currentQuickTextFile.name !== ""
                         ? self.currentQuickTextFile.name
-                        : quickTextInputField.attr(
+                        : $("#quick_text_dialog_text_input").attr(
                               "placeholder"
                           );
 
@@ -3962,22 +3960,12 @@ $(function () {
                 });
 
                 // update font of input field
-                quickTextInputField.css({
+                $("#quick_text_dialog_text_input").css({
                     "text-shadow": shadow,
                     color: previewFill,
                     "font-family": font,
                     "font-variant-ligatures": ligatures,
                 });
-                // update input placeholder color if stroked
-                let StrokedQuickTextPlaceholderClass = 'mrb-quicktext-stroked-placeholder';
-                if(isStroked){
-                    quickTextInputField.addClass(StrokedQuickTextPlaceholderClass);
-                }
-                // update input placeholder color if not stroked
-                if(!isStroked && quickTextInputField.hasClass(StrokedQuickTextPlaceholderClass)){
-                    quickTextInputField.removeClass(StrokedQuickTextPlaceholderClass);
-                }
-
                 $("#quick_text_dialog_font_name").text(font);
                 //                $("#quick_text_fill_brightness").val(fillColor);
                 //                $("#quick_text_stroke_color").val(strokeColor);
