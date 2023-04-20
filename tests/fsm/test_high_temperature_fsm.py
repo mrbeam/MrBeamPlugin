ffb190d5ab000c18be254a6d6c0e9ef02fb0a9e0
Josef-MrBeam <josef@mr-beam.org>
2023-04-20 17:01:28 +0200

    fix tests from acceptance of SWW-1158

--- tests/fsm/test_high_temperature_fsm.py
+++ tests/fsm/test_high_temperature_fsm.py
@@ -221,23 +221,6 @@ def test_transistion_from_dismissed_to_deactivated():
     )
 
 
-def test_transistion_from_warning_to_monitoring():
-    # Arrange
-    fsm = HighTemperatureFSM(MagicMock(), False, MagicMock())
-    fsm.start_monitoring()
-    fsm.warn()
-    assert fsm.warning.is_active
-    fsm._analytics_handler.add_high_temp_warning_state_transition = MagicMock()
-    # Act
-    fsm.dismiss()
-
-    # Assert
-    assert fsm.dismissed.is_active
-    fsm._analytics_handler.add_high_temp_warning_state_transition.assert_called_with(
-        "dismiss", "warning", "dismissed", False
-    )
-
-
 def test_feature_is_disabled_transition_from_monitoring_to_warning():
     # Arrange
     fsm = HighTemperatureFSM(MagicMock(), True, MagicMock())
@@ -323,6 +306,19 @@ def test_event_trigger_LASER_COOLING_TO_SLOW(high_temp_fsm):
     # Assert
     time.sleep(0.1)
     assert high_temp_fsm.warning.is_active
+
+
+def test_event_trigger_LASER_COOLING_TO_SLOW_to_critically(high_temp_fsm):
+    high_temp_fsm.start_monitoring()
+    high_temp_fsm.warn()
+    assert high_temp_fsm.warning.is_active
+
+    # Act
+    high_temp_fsm._event_bus.fire(MrBeamEvents.LASER_COOLING_TO_SLOW)
+
+    # Assert
+    time.sleep(0.1)
+    assert high_temp_fsm.critically.is_active
 
 
 def test_event_trigger_LASER_HIGH_TEMPERATURE(high_temp_fsm):
