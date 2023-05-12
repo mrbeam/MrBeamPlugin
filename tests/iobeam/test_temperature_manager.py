59fbc1dc598835e5f534398d8445b6d986df338b
Josef-MrBeam <81746291+Josef-MrBeam@users.noreply.github.com>
2023-05-12 09:10:16 +0200

    SW-3162 fix missing cooling difference in the cooling to slow event in analytics (#1752)
    * fix typo

--- tests/iobeam/test_temperature_manager.py
+++ tests/iobeam/test_temperature_manager.py
@@ -173,7 +173,7 @@ def test_handle_temp_fire_cooling_to_slow_event_second_threshold(temperature_man
     # Assert
     temperature_manager._event_bus.fire.assert_called_with(
         MrBeamEvents.LASER_COOLING_TO_SLOW,
-        dict(temp=49, cooling_differnece=1.0, cooling_time=approx(61, rel=1)),
+        dict(temp=49, cooling_difference=1.0, cooling_time=approx(61, rel=1)),
     )
 
 
@@ -193,7 +193,7 @@ def test_handle_temp_fire_cooling_to_slow_event_third_threshold(temperature_mana
         MrBeamEvents.LASER_COOLING_TO_SLOW,
         dict(
             temp=49,
-            cooling_differnece=1.0,
+            cooling_difference=1.0,
             cooling_time=approx(141, rel=0.01),
         ),
     )
