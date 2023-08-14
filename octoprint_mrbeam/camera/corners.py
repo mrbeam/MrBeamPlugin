f76f16cc4a84eb1b64ee1d5c9fdd24a038a0bbb6
khaledsherkawi <54568489+khaledsherkawi@users.noreply.github.com>
2023-08-14 15:58:37 +0200

    SW-3644 update material settings  and link in the MrBeamPlugin and translate (#1793)
    * SW-3644 Update material settings
    
    * SW-3644 Update material settings
    
    * SW-3644 Fix material settings bug
    
    * SW-3644 Revert this function rename as it is used via the camera plugin
    
    * Revert "SW-3644 Fix material settings bug"
    
    This reverts commit dba16f36cde7572558e3b657afe662edf7dad02b.
    
    * Revert "SW-3644 Update material settings"
    
    This reverts commit 3775e8a1cb2986a004a623c1284b94a95135d033.
    
    * Revert "SW-3644 Update material settings"
    
    This reverts commit fef609919fa32daaf51355a933387dd4149b5e5e.
    
    * SW-3644 Fix link
    
    * SW-3644 Modify text
    
    * SW-3644 Add shadow to tooltip
    
    * SW-3644 Translate added/modified strings
    
    * SW-3644 Add comment for clarification

--- octoprint_mrbeam/camera/corners.py
+++ octoprint_mrbeam/camera/corners.py
@@ -25,7 +25,8 @@ from octoprint_mrbeam.mrb_logger import mrb_logger
 _logger = mrb_logger("octoprint.plugins.mrbeam.camera.corners")
 
 # @logtime()
-def warp_image_by_corners(image, corners, zoomed_out=False):
+# DO NOT CHANGE - This is used by the camera plugin
+def warpImgByCorners(image, corners, zoomed_out=False):
     """Warps the region delimited by the corners in order to straighten it.
 
     :param image: takes an opencv image
