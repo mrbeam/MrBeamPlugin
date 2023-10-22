231e67105f44d98c9a597ac1dd90bdb4cc70d0bf
khaledsherkawi <khaled@mr-beam.org>
2023-10-22 20:35:00 +0200

    SW-3951 Add laser cutter profile service
    This class implements the profile parent class to be used for laser cutter profiles.

---
+++ octoprint_mrbeam/service/profile/laser_cutter_profile.py
@@ -0,0 +1,44 @@
+from octoprint_mrbeam.mrb_logger import mrb_logger
+from octoprint_mrbeam.service.profile.profile import ProfileService
+from octoprint_mrbeam.model.laser_cutter_profile import LaserCutterProfileModel
+from octoprint_mrbeam.constant.profile.laser_cutter.profile_1 import profile as default_profile
+
+# singleton instance of the LaserCutterProfileService class to be used across the application
+_instance = None
+
+
+def laser_cutter_profile_service(profile=default_profile):
+    """
+    Get or create a singleton instance of the LaserCutterProfileService.
+
+    This function is used to manage a singleton instance of the LaserCutterProfileService
+    class. It ensures that only one instance of the service is created and returned
+    during the program's execution.
+
+    Example Usage: laser_cutter_profile_service = laser_cutter_profile_service(plugin_instance)
+
+    Args:
+        profile (dict): The default profile of the laser cutter.
+
+    Returns:
+        _instance (LaserCutterProfileService): The singleton instance of the LaserCutterProfileService
+        class. If no instance exists, it creates one and returns it.
+    """
+    global _instance
+    if _instance is None:
+        _instance = LaserCutterProfileService(id="laser_cutter", profile=LaserCutterProfileModel(profile).data)
+    return _instance
+
+
+class LaserCutterProfileService(ProfileService):
+    """ Service class for laser cutter profile. """
+
+    def __init__(self, id, profile):
+        """Initialize laser cutter profile service.
+
+        Args:
+            id (str): The id of the laser cutter profile.
+            profile (dict): The profile of the laser cutter.
+        """
+        super(LaserCutterProfileService, self).__init__(id, profile)
+        self._logger = mrb_logger("octoprint.plugins.mrbeam.services.profile.laser_cutter_profile")
